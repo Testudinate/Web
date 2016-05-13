b87991b1bb5e31785170bf1e66ce7349bc99f448
Developer <klevtsovstanislav@gmial.com>
2016-05-13 23:35:07 +0300

    Update 1_6.py

--- 1_6.py
+++ 1_6.py
@@ -1,15 +1,12 @@
-import socket                                                                   
-import os
-
-s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)                          
-s.bind(('0.0.0.0', 2222))                                                       
-s.listen(1)
-while True:                                                            
-        conn, addr = s.accept()                                 
-        while True:                                 
-                data = conn.recv(1024)                                  
-                if not data: break                                      
-                conn.send(data)  
-                if data == 'close': break
-        print('close it')                                               
+import socket 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind(('0.0.0.0', 2222)) 
+s.listen(1) 
+while True:
+        conn, addr = s.accept()
+        while True:
+                data = conn.recv(1024)
+                if data == 'close':
+                        break
+                conn.send(data)
         conn.close()
